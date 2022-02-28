b1be9b3991b865949b3bdcb895d4ebd348a56815
Armand prevot <armand.prevot@etu.unice.fr>
2022-02-28 15:29:19 +0100

    adding console app client

---
+++ NFConsoleApp/NFConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string Id = "cringe";
+HubConnection connection = new HubConnectionBuilder()
+                .WithUrl($"http://localhost:8080/userhub/{Id}")
+                .Build();
+
+connection.Closed += async (error) => {
+    await Task.Delay(new Random().Next(0, 5) * 1000);
+    await connection.StartAsync();
+};
+
+connection.On<UserDTO>("ReceivedMessage", (user) => {
+    Console.WriteLine(JsonConvert.SerializeObject(user));
+});
+
+class UserDTO
+{
+    public string UserId { get; set; } = "";
+    public string StoreId { get; set; } = "";
+    public long Timestamp { get; set; }
+}
+
