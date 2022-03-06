dfaae267e108dc125dc2153a9dc547a5abd99b71
LGuillaumet <leo.guillaumet@etu.unice.fr>
2022-03-06 22:38:34 +0100

    add select storeId

--- NFCFront/src/views/Home.js
+++ NFCFront/src/views/Home.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { HubConnectionBuilder }  from '@microsoft/signalr';
+import Select from 'react-select'
+import { HubConnectionBuilder } from '@microsoft/signalr';
 import { useTable } from 'react-table'
 
 import './Home.css';
-
-const connection = new HubConnectionBuilder()
-    .withUrl("http://localhost:8080/storehub?storeId=toto")
-    .build();
 
 function Table({ columns, data }) {
     const {
@@ -19,38 +16,44 @@ function Table({ columns, data }) {
         columns,
         data,
     })
-    
     // Render the UI for your table
     return (
-        <table {...getTableProps()} className="styled-table">
-        <thead>
-            {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                ))}
-            </tr>
-            ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-            {rows.map((row, i) => {
-            prepareRow(row)
-            return (
-                <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-                </tr>
-            )
-            })}
-        </tbody>
-        </table>
+        <>
+            <table {...getTableProps()} className="styled-table">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
     )
-    }
+}
 
 export const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [users, setUsers] = useState([]);
+    const [storeId, setStoreId] = useState('');
+    const [connection, setConnection] = useState(null);
+
+
 
     const columns = React.useMemo(() => [
         {
@@ -74,41 +77,61 @@ export const Home = () => {
     };
 
     useEffect(() => {
-        connection.start().then(() => {
-            setIsLoading(false);
-            connection.on("ReceivedMessage", (data) => {
-                setUsers(data.map((d) => {
-                    return {
-                        userId: d.userId,
-                        inTimestamp: parseTimeStamp(d.inTimestamp),
-                    }
-                }));
+        if (storeId) {
+            setConnection(new HubConnectionBuilder()
+                .withUrl(`http://localhost:8080/storehub?storeId=${storeId.value}`)
+                .build());
+        }
+    }, [storeId]);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start().then(() => {
+                setIsLoading(false);
+                connection.on("ReceivedMessage", (data) => {
+                    setUsers(data.map((d) => {
+                        return {
+                            userId: d.userId,
+                            inTimestamp: parseTimeStamp(d.inTimestamp),
+                        }
+                    }));
+                });
+                connection.invoke("GetBaseList").then((data) => {
+                    setUsers(data.map((d) => {
+                        return {
+                            userId: d.userId,
+                            inTimestamp: parseTimeStamp(d.inTimestamp),
+                        }
+                    }));
+                });
             });
-            connection.invoke("GetBaseList").then((data) => {
-                setUsers(data.map((d) => {
-                    return {
-                        userId: d.userId,
-                        inTimestamp: parseTimeStamp(d.inTimestamp),
-                    }
-                }));
-            });
-        });
-        return () => {
-            setUsers([]);
-            connection.stop();
-        };
-    }, []);
+            return () => {
+                setUsers([]);
+                connection.stop();
+            };
+        }
+    }, [connection]);
+
+    const options = [
+        { label: "Fnac", value: "Fnac" },
+        { label: "Monoprix", value: "Monoprix" },
+        { label: "Carrefour", value: "Carrefour" },
+        { label: "Darty", value: "Darty" }
+    ]
 
     return (
         <div className="w-100">
+            <h1>Selectionner un magasin</h1>
+            <Select options={options} value={storeId} onChange={setStoreId} />
             {isLoading ? (
                 <p>Connection...</p>
             ) : (
                 <>
+                    <h2>Magasin : {storeId.value} </h2>
                     <p>Nombre de Client: {users.length}</p>
                     <Table columns={columns} data={users} />
                 </>
-            )}           
+            )}
         </div>
     );
 };
